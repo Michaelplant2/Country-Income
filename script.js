d85c6bceeba2003c708a87195210278b9ee3d61a
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-02 15:24:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const url = "http://api.worldbank.org/v2/country/?format=json";
+
+const main = document.querySelector("main");
+const div = document.getElementById("country-list");
+const prevPg = document.getElementById("prevPage");
+const nextPg = document.getElementById("nextPg");
+
+getCountries();
+
+async function getCountries() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    listData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function listData(data) {
+  data[1].map(function (country) {
+    const countryName = `
+        <li id="country">${country.name}</li>
+      `;
+    const ul = document.createElement("ul");
+    ul.innerHTML = countryName;
+    div.appendChild(ul);
+    ul.addEventListener("click", () => {
+      ul.innerHTML = `
+        <li><strong>Region:</strong>${country.region.value}</li>
+        <li><strong>Capital:</strong>${country.capitalCity}</li>
+        <li><strong>Income:</strong>${country.incomeLevel.value}</li>
+      `;
+      if (country.incomeLevel.value === "High income") {
+        ul.style.backgroundColor = "green";
+      } else if (
+        country.incomeLevel.value === "Lower middle income" ||
+        country.incomeLevel.value === "Upper middle income"
+      ) {
+        ul.style.backgroundColor = "yellow";
+      } else if (country.incomeLevel.value === "Low income") {
+        ul.style.backgroundColor = "red";
+      } else {
+
+      }
+    });
+  });
+  main.appendChild(div);
+}
+
+function pageSwap() {
+    
+}
+
+// function clickInfo() {
+//     ul.innerHTML = `
+//         <li><strong>Region:</strong>${country.region.value}</li>
+//         <li><strong>Capital:</strong>${country.capitalCity}</li>
+//         <li><strong>Income:</strong>${country.incomeLevel.value}</li>
+//       `;
+//       if (country.incomeLevel.value === "High income") {
+//         ul.style.backgroundColor = "green";
+//       } else if (
+//         country.incomeLevel.value === "Lower middle income" ||
+//         country.incomeLevel.value === "Upper middle income"
+//       ) {
+//         ul.style.backgroundColor = "yellow";
+//       } else if (country.incomeLevel.value === "Low income") {
+//         ul.style.backgroundColor = "red";
+//       } else {
+
+//       }
+// }
